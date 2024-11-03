c135807d87a0c3812a889a5873c9f472cc89f3a4
Darcey Mckelvey <darceymckelvey@gmail.com>
2024-11-03 18:20:12 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -19,6 +19,21 @@ const mockUsers = [
             id: 3,
             username: 'nightraven',
             displayName: "Raven"
+        },
+        {
+            id: 4,
+            username: 'marilyn',
+            displayName: "Mary"
+        },
+        {
+            id: 5,
+            username: 'dustbringer',
+            displayName: "Dust"
+        },
+        {
+            id: 6,
+            username: 'Oysters',
+            displayName: "Oyster"
         }
     ]
 
@@ -27,13 +42,26 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/users', (request, response) => {
-    response.send(mockUsers);
+    console.log(request.query);
+    const { query: {filter, value},
+    } = request;
+
+    if (filter && value) return response.send(
+        mockUsers.filter((user) => user[filter].includes(value))
+    ); 
+
+    return response.send(mockUsers);
 });
 
 app.get('/api/users/:id', (request, response) => {
     console.log(request.params);
-    const parseId = parseInt(request.params.id);
-    console.log(parseId);
+    const parsedId = parseInt(request.params.id);
+    console.log(parsedId);
+    if (isNaN(parsedId)) return response.status(400).send({msg: "Bad Request. Invalid ID."});
+
+    const findUser = mockUsers.find((user) => user.id === parsedId);
+    if (!findUser) return response.sendStatus(404);
+    return response.send(findUser);
 });
 
 app.get('/api/products', (request, response) => {
