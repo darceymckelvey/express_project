bc9a2a6a6e080f57cc62178f11f1a859e49ad4d6
Darcey Mckelvey <darceymckelvey@gmail.com>
2024-11-04 01:03:50 -0400

    index.mjs
    post, get, put, patch, delete and middleware, node , express

--- index.mjs
+++ index.mjs
@@ -2,6 +2,29 @@ import express from 'express';
 
 const app = express();
 
+// middleware
+app.use(express.json());
+
+const loggingMiddleware = (request, response, next) => {
+    console.log(`${request.method} - ${request.url}`);
+    next();
+}
+
+app.use(loggingMiddleware);
+
+const resolveIndexByUserId = (request, response, next) => {
+    const { 
+        params:{ id }
+    } = request;
+
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) return response.sendStatus(400);
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId); 
+    if (findUserIndex === -1) return response.sendStatus(404);
+    request.findUserIndex = findUserIndex;
+    next();
+}
+
 const PORT = process.env.PORT || 3000;
 
 const mockUsers = [
@@ -37,7 +60,13 @@ const mockUsers = [
         }
     ]
 
-app.get('/', (request, response) => {
+app.get(
+    '/',
+    (request, response, next) => { 
+        console.log('Base URL');
+        next();
+     },
+    (request, response) => {
     response.status(200).send({msg: "Hello world!"});
 });
 
@@ -53,13 +82,17 @@ app.get('/api/users', (request, response) => {
     return response.send(mockUsers);
 });
 
-app.get('/api/users/:id', (request, response) => {
-    console.log(request.params);
-    const parsedId = parseInt(request.params.id);
-    console.log(parsedId);
-    if (isNaN(parsedId)) return response.status(400).send({msg: "Bad Request. Invalid ID."});
+app.post('/api/users', (request, response) => {
+    console.log(request.body);
+    const { body } = request; 
+    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+    mockUsers.push(newUser);
+    return response.status(201).send(newUser);
+});
 
-    const findUser = mockUsers.find((user) => user.id === parsedId);
+app.get('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { findUserIndex } = request;
+    const findUser = mockUsers[findUserIndex];
     if (!findUser) return response.sendStatus(404);
     return response.send(findUser);
 });
@@ -76,4 +109,25 @@ app.get('/api/products', (request, response) => {
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`);
+});
+
+// PUT updates all field so if not provided iut will be updated without it
+app.put('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { body, findUserIndex } = request;
+    mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
+    return response.sendStatus(204);
+});
+
+// PATCH you can updat username and all unprovided keys stay the same
+app.patch('/api/users/:id', resolveIndexByUserId, (request, response) => {
+    const { body, findUserIndex } = request;
+    mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+    return response.sendStatus(204);
+});
+
+app.delete('/api/users/:id',resolveIndexByUserId, (request, response) => {
+    const { findUserIndex } = request;
+
+    mockUsers.splice(findUserIndex, 1);
+    return response.sendStatus(204);
 });
